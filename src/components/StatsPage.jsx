63426c8affe652b81575701524ce98e3d65f48b3
CyberSupreme3642 <nightshade732@gmail.com>
2024-04-24 03:32:43 -0400

    wip

--- src/components/StatsPage.jsx
+++ src/components/StatsPage.jsx
@@ -9,7 +9,61 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 function StatsPage(){
     const [filterStartTime, setStartTime] = useState(null);
     const [filterEndTime, setEndTime] = useState(null)
+    const [visits, setVisits] = useState([]);
+    const [filteredVisits, setFilteredVisits] = useState([]);
 
+    useEffect(() => {
+        getAllVisits();
+    }, [])
+
+    useEffect(() => {
+        filterJobs();
+    }, [filteredVisits])
+
+
+    async function getAllVisits() {
+        await fetch('http://localhost:5050/visit/', {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                setVisits(json);
+                console.log(json);
+            })
+            .catch((err) => console.error(err))
+    }
+
+    function filterJobs() {
+        var filter = [];
+        if (filterStartTime === null && filterEndTime === null) {
+            // setFilteredVisits(visits);
+            filter = visits;
+        } else {
+            for (var i = 0; i < visits.length; i++) {
+                    if (dateInRange(visits[i]['date'], filterStartTime, filterEndTime) === true)
+                        filter.push(visits[i]);
+            }
+        }
+        console.log(filter);
+        setFilteredVisits(filter);
+    }
+
+     /**
+     * 
+     * @param {string} testDate 
+     * @param {dayjs} startDate 
+     * @param {dayjs} endDate 
+     * @returns 
+     */
+     function dateInRange(testDate, startDate, endDate) {
+        if (startDate === null && endDate === null)
+            return true;
+        if (startDate !== null && dayjs(testDate).isBefore(startDate))
+            return false;
+        if (endDate !== null && dayjs(testDate).isAfter(endDate))
+            return false;
+        return true;
+    }
 
     return(
         <div>
@@ -22,11 +76,24 @@ function StatsPage(){
                         <Grid item xs={4}>
                             <InputLabel id= 'start-date-picker'>Start Date</InputLabel>
                             <DatePicker>
+                                fullWidth
                                 value={filterStartTime}
                                 onChange={(newValue) => {
                                     console.log(newValue);
 
                                     setStartTime(newValue)
+                                }}
+                            </DatePicker>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <InputLabel id= 'end-date-picker'>End Date</InputLabel>
+                            <DatePicker>
+                                fullWidth
+                                value={filterEndTime}
+                                onChange={(newValue) => {
+                                    console.log(newValue);
+
+                                    setEndTime(newValue)
                                 }}
                             </DatePicker>
                         </Grid>
