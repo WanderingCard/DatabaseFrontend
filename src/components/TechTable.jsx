4b08b2cf3badc9357bfd0f697f60a9a6a0b08ec0
mdevito4 <michaeldevito28@gmail.com>
2024-04-24 11:15:13 -0400

    possible fixes

--- src/components/TechTable.jsx
+++ src/components/TechTable.jsx
@@ -1,15 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Grid, InputLabel, MenuItem, Paper, Select, Table, TableCell, TableHead, TableRow, Typography } from "@mui/material";
-import { useState, useEffect } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import dayjs from 'dayjs';
+import DatePicker from '@mui/lab/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-
 
 function TechTable() {
     const [customers, setCustomers] = useState([]);
@@ -20,7 +20,7 @@ function TechTable() {
     const [filterEndTime, setEndTime] = useState(null);
     const [filteredVisits, setFilteredVisits] = useState([]);
     const [visitsLoaded, setLoaded] = useState(false);
-    const [selectedTechnician, setSelectedTechnician] = useState('no value');
+    const [selectedTechnician, setSelectedTechnician] = useState('');
 
     useEffect(() => {
         getAllCustomers();
@@ -31,7 +31,7 @@ function TechTable() {
 
     useEffect(() => {
         filterJobs();
-    }, [visits, cars, visits])
+    }, [visits, cars, selectedTechnician, filterStartTime, filterEndTime])
 
     useEffect(() => {
         if (filteredVisits.length > 0)
@@ -39,84 +39,54 @@ function TechTable() {
     }, [filteredVisits])
 
     async function getAllCustomers() {
-        await fetch('http://localhost:5050/customers/', {
-            method: 'GET'
-        })
-            .then((response) => response.json())
-            .then((json) => {
-                setCustomers(json);
-                console.log(json);
-            })
-            .catch((err) => console.error(err))
+        try {
+            const response = await axios.get('http://localhost:5050/customers/');
+            setCustomers(response.data);
+        } catch (error) {
+            console.error('Error fetching customers:', error);
+        }
     }
 
     async function getAllCars() {
-        await fetch('http://localhost:5050/cars/', {
-            method: 'GET'
-        })
-            .then((response) => response.json())
-            .then((json) => {
-                setCars(json);
-                console.log(json);
-            })
-            .catch((err) => console.error(err))
+        try {
+            const response = await axios.get('http://localhost:5050/cars/');
+            setCars(response.data);
+        } catch (error) {
+            console.error('Error fetching cars:', error);
+        }
     }
 
     async function getAllVisits() {
-        await fetch('http://localhost:5050/visit/', {
-            method: 'GET'
-        })
-            .then((response) => response.json())
-            .then((json) => {
-                setVisits(json);
-                console.log(json);
-            })
-            .catch((err) => console.error(err))
+        try {
+            const response = await axios.get('http://localhost:5050/visit/');
+            setVisits(response.data);
+        } catch (error) {
+            console.error('Error fetching visits:', error);
+        }
     }
 
     async function getAllTechs() {
-        await fetch('http://localhost:5050/technicians/', {
-            method: 'GET'
-        })
-        .then((response) => response.json())
-        .then((json) => setTechnicians(json))
-        .catch((err) => console.error(err))
+        try {
+            const response = await axios.get('http://localhost:5050/technicians/');
+            setTechnicians(response.data);
+        } catch (error) {
+            console.error('Error fetching technicians:', error);
+        }
     }
 
-    useEffect(() => {
-        setSelectedTechnician('no value');
-        console.log(technicians)
-    }, [technicians])
-
-    useEffect(() => {
-        console.log(selectedTechnician);
-        filterJobs();
-    }, [selectedTechnician, filterStartTime, filterEndTime])
-
     function filterJobs() {
-        var filter = [];
-        if (selectedTechnician === 'no value' && filterStartTime === null && filterEndTime === null) {
-            // setFilteredVisits(visits);
+        let filter = [];
+        if (selectedTechnician === '' && filterStartTime === null && filterEndTime === null) {
             filter = visits;
         } else {
-            for (var i = 0; i < visits.length; i++) {
-                if(selectedTechnician === 'no value' || visits[i]['job'].some(job => job.technician_id === selectedTechnician)){
-                    if (dateInRange(visits[i]['date'], filterStartTime, filterEndTime) === true)
-                        filter.push(visits[i]);
-                }
-            }
+            filter = visits.filter(visit =>
+                (selectedTechnician === '' || visit.job.some(job => job.technician_id === selectedTechnician)) &&
+                dateInRange(visit.date, filterStartTime, filterEndTime)
+            );
         }
-        console.log(filter);
         setFilteredVisits(filter);
     }
 
-    /**
-     * 
-     * @param {string} testDate 
-     * @param {dayjs} startDate 
-     * @param {dayjs} endDate 
-     * @returns 
-     */
     function dateInRange(testDate, startDate, endDate) {
         if (startDate === null && endDate === null)
             return true;
@@ -128,73 +98,29 @@ function TechTable() {
     }
 
     function getCarData(carId) {
-        // console.log(carId);
-        for (var i = 0; i < cars.length; i++) {
-            // console.log(cars[i]._id)
-            if (cars[i]._id === carId)
-                return cars[i];
-        }
-        return null;
+        return cars.find(car => car._id === carId) || {};
     }
 
     function getCustomerName(custId) {
-        for (var i = 0; i < customers.length; i++) {
-            if (customers[i]._id === custId)
-                return customers[i].fname + ' ' + customers[i].lname;
-        }
-        return null;
-    }
-
-    /**
-     * 
-     * @param {Array[object]} array - an array of visits, must contain the car attribute
-     */
-    function groupByCar(array) {
-        var carVisits = {};
-        for (var i = 0; i < array.length; i++) {
-            if(array[i].car in carVisits) {
-                var newArray = [...carVisits[array[i].car], array[i]];
-                carVisits = {...carVisits, [array[i].car]: newArray};
-            } else {
-                carVisits = {...carVisits, [array[i].car]: [array[i]]};
-            }
-            console.log(array[i].car, carVisits)
-        }
-
-        var sortedArray = [];
-
-        console.log(carVisits)
-
-        for (const car in carVisits) {
-            const carSort = carVisits[car].sort((a, b) => a.car.localeCompare(b.car));
-            sortedArray = sortedArray.concat(carSort);
-        }
-        return sortedArray;
+        return (customers.find(customer => customer._id === custId) || {}).fname + ' ' + (customers.find(customer => customer._id === custId) || {}).lname;
     }
 
     function getTechName(techId) {
-        for(var i = 0; i < technicians.length; i++) {
-            if (technicians[i]._id === techId) {
-                return technicians[i].firstname + ' ' + technicians[i].lastname;
-            }
-        }
-        return "N/A";
+        return (technicians.find(tech => tech._id === techId) || {}).firstname + ' ' + (technicians.find(tech => tech._id === techId) || {}).lastname;
     }
 
     function getTechSales() {
-        var salesTotal = 0;
-        for(var i = 0; i < filteredVisits.length; i++) {
-            if(filteredVisits[i].job.some(job => job.technician_id === selectedTechnician)) {
-                for (var j = 0; j < filteredVisits[i].job.length; j++) {
-                    if (filteredVisits[i].job[j].technician_id === selectedTechnician) {
-                        var serviceCost = parseFloat(filteredVisits[i].job[j].service.cost);
-                        if(!isNaN(serviceCost)){
-                            salesTotal += serviceCost;
-                        }
+        let salesTotal = 0;
+        filteredVisits.forEach(visit => {
+            visit.job.forEach(job => {
+                if (job.technician_id === selectedTechnician) {
+                    const serviceCost = parseFloat(job.service.cost);
+                    if (!isNaN(serviceCost)) {
+                        salesTotal += serviceCost;
                     }
                 }
-            }
-        }
+            });
+        });
         return salesTotal;
     }
 
@@ -207,14 +133,12 @@ function TechTable() {
                     labelId='TechnicianSelectBox'
                     id='ServiceSelect'
                     value={selectedTechnician}
-                    onChange={(event) => {
-                        setSelectedTechnician(event.target.value);
-                        console.log(event.target.value);
-                    }}
+                    onChange={(event) => setSelectedTechnician(event.target.value)}
                 >
-                    {["", ...technicians].map((technicians) => (
-                        <MenuItem key={technicians._id ? technicians._id : 'no_value'} value={technicians._id ? technicians._id : 'no value'}>
-                            {technicians._id ? technicians.firstname + " " + technicians.lastname : '-- Select Technician --'}
+                    <MenuItem value="">-- Select Technician --</MenuItem>
+                    {technicians.map(technician => (
+                        <MenuItem key={technician._id} value={technician._id}>
+                            {technician.firstname} {technician.lastname}
                         </MenuItem>
                     ))}
                 </Select>
@@ -225,11 +149,7 @@ function TechTable() {
                     <DatePicker
                         fullWidth
                         value={filterStartTime}
-                        onChange={(newValue) => {
-                            console.log(newValue);
-
-                            setStartTime(newValue)
-                        }}
+                        onChange={(newValue) => setStartTime(newValue)}
                     />
                 </Grid>
                 <Grid item xs={4}>
@@ -237,10 +157,7 @@ function TechTable() {
                     <DatePicker
                         fullWidth
                         value={filterEndTime}
-                        onChange={(newValue) => {
-                            console.log(newValue);
-                            setEndTime(newValue)
-                        }}
+                        onChange={(newValue) => setEndTime(newValue)}
                     />
                 </Grid>
             </LocalizationProvider>
@@ -253,56 +170,49 @@ function TechTable() {
                         <TableCell>Date and Time</TableCell>
                         <TableCell>Service Name</TableCell>
                         <TableCell>Service Cost</TableCell>
-                        {selectedTechnician === 'no value' && <TableCell>Technician</TableCell>}
+                        {selectedTechnician === '' && <TableCell>Technician</TableCell>}
                     </TableHead>
                     {visitsLoaded === false ?
                         <Typography>Loading Visits</Typography> :
-                        groupByCar(filteredVisits).map((job) => {
-                            var carData = getCarData(job.car);
-                            console.log(carData);
-                            return (
-                                <>
-                                    <TableRow>
-                                       <TableCell rowSpan={job.job.length}>{getCustomerName(job.customer)}</TableCell>
-                                        <TableCell rowSpan={job.job.length}>
-                                            {carData.licensePlate}
-                                        </TableCell>
-                                        <TableCell rowSpan={job.job.length}>
-                                            {carData.model}
-                                        </TableCell>
-                                        <TableCell rowSpan={job.job.length}>
-                                            {dayjs.tz(job.date, 'America/New_York').format()}
+                        filteredVisits.map((visit, index) => (
+                            <React.Fragment key={index}>
+                                {visit.job.map((job, i) => (
+                                    <TableRow key={`${index}-${i}`}>
+                                        {i === 0 && (
+                                            <>
+                                                <TableCell rowSpan={visit.job.length}>
+                                                    {getCustomerName(visit.customer)}
+                                                </TableCell>
+                                                <TableCell rowSpan={visit.job.length}>
+                                                    {getCarData(visit.car).licensePlate}
+                                                </TableCell>
+                                                <TableCell rowSpan={visit.job.length}>
+                                                    {getCarData(visit.car).model}
+                                                </TableCell>
+                                                <TableCell rowSpan={visit.job.length}>
+                                                    {dayjs.tz(visit.date, 'America/New_York').format()}
+                                                </TableCell>
+                                            </>
+                                        )}
+                                        <TableCell>
+                                            {job.service.serviceName}
                                         </TableCell>
                                         <TableCell>
-                                            {job.job[0].service.serviceName}
+                                            {job.service.cost}
                                         </TableCell>
-                                        <TableCell>
-                                            {job.job[0].service.cost}
-                                        </TableCell>
-                                        {selectedTechnician === 'no value' &&  <TableCell>
-                                            {getTechName(job.job[0].technician_id)}
-                                        </TableCell>}
+                                        {selectedTechnician === '' && (
+                                            <TableCell>
+                                                {getTechName(job.technician_id)}
+                                            </TableCell>
+                                        )}
                                     </TableRow>
-                                    {job.job.slice(1).map((service) => (
-                                        <TableRow>
-                                            <TableCell>
-                                                {service.service.serviceName}
-                                            </TableCell>
-                                            <TableCell>
-                                                {service.service.cost}
-                                            </TableCell>
-                                            {selectedTechnician === 'no value' && <TableCell>
-                                                {getTechName(service.technician_id)}
-                                            </TableCell>}
-                                        </TableRow>
-                                    ))}
-                                </>
-                            )
-                        })
+                                ))}
+                            </React.Fragment>
+                        ))
                     }
                 </Table>
             </Grid>
-            {selectedTechnician !== 'no value' && (
+            {selectedTechnician !== '' && (
                 <Grid item xs={4}>
                     <Typography>Total Sales: {getTechSales()}</Typography>
                 </Grid>
@@ -310,5 +220,4 @@ function TechTable() {
         </Grid >
     )
 }
-
 export default TechTable;
